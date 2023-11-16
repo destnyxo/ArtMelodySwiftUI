de6ffe524de56871e51b0b649563ebea40eeac74
KRainer53 <148348712+KRainer53@users.noreply.github.com>
2023-11-16 11:33:30 -0500

    chap 4

---
+++ Abuut me/Abuut me/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Abuut me
+//
+//  Created by Keith b. Rainer on 11/15/23.
+//
+
+import SwiftUI
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text ("About Me")
+        }
+    }
+    
+            
+        }
+    
+    
+
